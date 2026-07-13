85ae258a4018dee2e50798addbb8dd457ff3f188
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-07-14 01:18:35 +0530

    feat: connect MongoDB using mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+
 
 dotenv.config();
 
@@ -17,6 +19,11 @@ app.get("/api/health", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
